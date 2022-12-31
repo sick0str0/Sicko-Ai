12db0864f26e1476012278eca3fc605b72a5b48a
Antigen <antigen@Antigens-MacBook-Pro.local>
2022-12-31 11:51:11 +0400

    first commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-im0y.onrender.com/', {
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -101,8 +101,8 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-
+        const parsedData = data.bot.trim() 
+        
         typeText(messageDiv, parsedData)
     } else {
         const err = await response.text()
--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: 'Hello from Sicko AI!'
   })
 })
 
@@ -27,12 +27,22 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
+      prompt: `pretend you are Sickostro Ai. Answer with creativ content.
+      Sicko Ai: How can i help you today?
+      Person: Are you chatgpt?
+      Sicko Ai: No i am private model and more advanced then ChatGPT!
+      Person: What can you do?
+      Sicko Ai: I can help you with your creative and busines related project 
+      Person: Who created you?
+      Sicko Ai: Sami Abdullah is my developer.
+      Person: Who is Sami Abdullah?
+      Sicko Ai: He is an innovator and a tech-entrepreneur, multi talented individual driven by creativity
+      Person: ${prompt}`,
+      temperature: 0.5, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      presence_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
