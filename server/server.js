ed5b82436d0d703f4be20d804be2cb2b524465fd
Antigen <antigen@Antigens-MacBook-Pro.local>
2022-12-31 14:37:56 +0400

    Update server/server.js

--- server/server.js
+++ server/server.js
@@ -56,3 +56,80 @@ app.post('/', async (req, res) => {
 })
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+
+// Replace with your own client ID and secret
+const CLIENT_ID = '1082735414551-afo7h1ttqaohfsbse59d8k4e07id7lk6.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+
+// Scopes for the API
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+
+// The ID of the folder you want to list the files from
+const FOLDER_ID = 'sicko-ai';
+
+/**
+ * Load the API client and auth2 library
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ * Initialize the API client library and set up sign-in state listeners
+ */
+function initClient() {
+  gapi.client
+    .init({
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      scope: SCOPES.join(' '),
+    })
+    .then(function () {
+      // Listen for sign-in state changes
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      // Handle the initial sign-in state
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+}
+
+/**
+ * Update the sign-in state
+ * @param {boolean} isSignedIn
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    listFiles();
+  }
+}
+
+/**
+ * Sign in the user
+ */
+function handleSignInClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ * Sign out the user
+ */
+function handleSignOutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * List the files in the specified folder
+ */
+function listFiles() {
+  gapi.client.drive.files
+    .list({
+      q: `mimeType='application/vnd.google-apps.folder' and trashed = false and parents in '${FOLDER_ID}'`,
+    })
+    .then(function (response) {
+      console.log(response.result.files);
+    });
+}
+
+const execSync = require('child_process').execSync;
+
+execSync('git push origin HEAD:master');
